83b50b26bdc6aa63d3e21c3978e3b4d52acdff1a
achmadridho <Souleater999>
2018-05-04 17:11:19 +0700

    Upload Images

--- routes/file.js
+++ routes/file.js
@@ -18,6 +18,7 @@ let storage = multer.diskStorage({
 
 });
 let upload = multer({ storage: storage }).single('docFile');
+let uploadImage = multer({ storage: storage }).single('image');
 
 router.post('/upload/single',function(req,res,next){
     try {
@@ -31,7 +32,7 @@ router.post('/upload/single',function(req,res,next){
                if(req.file){
                    let file =path.join(__dirname, '../public/temps/'+req.file.filename);
                    console.log(file);
-                   let ftpDestFolderAndFileName="STORAGE/BANWASLU/"+req.file.filename;
+                   let ftpDestFolderAndFileName="STORAGE/BANWASLU/DOCUMENTS/"+req.file.filename;
                    let fileHostingPath="http://filehosting.pptik.id/"+ftpDestFolderAndFileName;
 
                    let Ftp = new JSFtp({
@@ -59,6 +60,52 @@ router.post('/upload/single',function(req,res,next){
                    res.status(200).send(commonMsg.upload_failed);
                }
            }
+        });
+    }catch (err){
+        console.log(err)
+        res.status(200).send(commonMsg.upload_failed);
+    }
+});
+router.post('/upload/image',function(req,res,next){
+    try {
+        console.log(req.file);
+        uploadImage(req,res,function (err) {
+            if(err){
+                console.log(err);
+                res.status(200).send(commonMsg.upload_failed);
+            }
+            else {
+                if(req.file){
+                    let file =path.join(__dirname, '../public/temps/'+req.file.filename);
+                    console.log(file);
+                    let ftpDestFolderAndFileName="STORAGE/BANWASLU/IMAGES/"+req.file.filename;
+                    let fileHostingPath="http://filehosting.pptik.id/"+ftpDestFolderAndFileName;
+
+                    let Ftp = new JSFtp({
+                        host:ftp.pptik.host,
+                        port: 21,
+                        user: ftp.pptik.user,
+                        pass: ftp.pptik.pass
+                    });
+                    Ftp.put(file, ftpDestFolderAndFileName, function(err) {
+                        if (err) console.log(err);
+                        else {
+                            fs.unlink(file,(err => {
+                                if(err)console.log(err);
+                            }));
+                            let imageDetail={originalname:req.file.originalname, filename:req.file.filename, http_path:fileHostingPath,type:"Image",size:req.file.size,encoding:req.file.encoding};
+                            console.log(imageDetail);
+                            let result=commonMsg.success_upload;
+                            result.results=imageDetail;
+                            Ftp.destroy();
+                            res.status(200).send(result);
+                        }
+                    });
+                }else {
+                    console.log('upload failed');
+                    res.status(200).send(commonMsg.upload_failed);
+                }
+            }
         });
     }catch (err){
         console.log(err)
